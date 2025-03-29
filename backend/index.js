3478c33586625d699de0d424c37b01acf598e647
Jaswanth5996 <jaswanth.140760@gmail.com>
2025-03-29 05:33:29 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,7 @@ app.use((err, req, res, next) => {
 
 // ✅ Server port setup
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
