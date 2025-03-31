a294e3109d3ca0733942bf20d4b1f856a2f53699
Jaswanth5996 <jaswanth.140760@gmail.com>
2025-03-31 07:00:54 +0530

    new

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
-const { createUser, getUserByPhoneNumber } = await import('../models/User.js');
+import { createUser, getUserByPhoneNumber } from '../models/User.js';
 
-const registerUser = async (req, res) => {
+export const registerUser = async (req, res) => {
   const { name, phoneNumber, lat, lng } = req.body;
 
   // ✅ Validate input fields
@@ -29,4 +29,4 @@ const registerUser = async (req, res) => {
   }
 };
 
-export default { registerUser };
+export default registerUser;
