49646f4d71571f6ae09f1e26c4ffc0671d28fe10
Jaswanth5996 <jaswanth.140760@gmail.com>
2025-03-31 06:57:54 +0530

    new

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
 import dotenv from 'dotenv';
-
 
 dotenv.config();
 
+const { Pool } = pkg;
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
