f17f12b7442d5db6e80a6452f9319a4002ad7513
Janga Jayanth <131176490+Jayanth490@users.noreply.github.com>
2025-03-31 10:38:12 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { registerUser, getLocationByPhoneNumber } from '../controllers/userController.js';
+import { registerUser, getUserLocation } from '../controllers/userController.js'; // Ensure the function is imported
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
     res.send('✅ User API is working!');
 });
 router.post('/register', registerUser);
-router.get('/location/:phoneNumber', getLocationByPhoneNumber);
+
+router.get('/location/:phone', getUserLocation);
 
 export default router;
